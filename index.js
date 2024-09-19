f98d81014ddd3b9ea5b6ce85c565ba0bfe537804
floresantonella <floresantonella972@gmail.com>
2024-09-18 21:05:12 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,108 @@
+//Punto 2
+// var text =" hola mundo <br>"
+// document.write(text)
+
+//punto 3
+// var numero1 = 3
+// var numero2 = 5
+// suma = numero1 + numero2
+// document.write(suma)
+
+//punto 4
+// var nombre = prompt ("ingrese su nombre:")
+// document.write("<br>hola: " + nombre)
+
+// //punto 5
+// var num1= Number (prompt ("ingrese un numero: "))
+// var num2= Number (prompt ("ingrese otro numero:" ))
+//  var suma= num1 + num2 
+//  document.write ("<br>resultado:" ,suma)
+
+//punto 6
+// var num1= Number.parseInt(prompt("ingrese un numero"))
+// var num2= Number.parseInt(prompt("ingrese otro numero"))
+// if (num1> num2){
+//     document.write("el mayor es: " , num1)
+// }else{ 
+//     document.write("el mayor es: " , num2)
+// }
+
+//punto 7
+// var num1= Number.parseInt(prompt("ingrese un numero"))
+// var num2= Number.parseInt(prompt("ingrese un numero"))
+// var num3= Number.parseInt(prompt("ingrese un numero"))
+// if(num1 > num2 && num1 > num3 ){
+//     document.write("el mayores:" , num1)
+// }else if (num2 > num1 && num2 > num3){
+//     document.write("el mayor es:" , num2)
+// }else{
+//     document.write("el mayor:" , num3)
+// }
+
+
+//punto 8
+// var num = Number(prompt("ingrese un numero"))
+// if (num % 2 == 0){
+//     document.write("el numero ingresado es divisible por 2:" , num)
+// } else{
+//     document.write("el numero ingresado no es divisible por 2:" , num)
+// }
+
+//punto 9
+// var num = Number.parseInt(prompt("ingrese un numero"))
+// if (num % 2 == 0){
+//     document.write("el numero ingresado es par:" , num)
+// }else{
+//     document.write("el numero ingresado es impar:" , num)
+// }
+
+//punto 10
+// var nota = Number(prompt("ingrese un nota:"))
+// if (nota >= 5){
+//     document.write("el alumno aprobo:" , nota)
+// }else{
+//     document.write("el alumno desaprobo:" , nota)
+// }
+
+// punto 11
+// let num_mes = (prompt("ingrese un numero del 1 al 12"))
+// if (num_mes == 1 ){
+//       document.write("Enero")
+// }else if (num_mes == 2){
+//       document.write("Febrero")
+// }else if (num_mes == 3){
+//       document.write("Marzo")
+// }else if (num_mes == 4){
+//   document.write("Abril")
+// }else if(num_mes == 5){
+//   document.write("Mayo")
+// }else if (num_mes == 6){
+//   document.write("Junio")
+// }else if (num_mes == 7){
+//   document.write("Julio")
+// }else if (num_mes == 8){
+//   document.write("Agosto")
+// }else if (num_mes == 9){
+//   document.write("Septiembre")
+// }else if (num_mes == 10){
+//   document.write("Octubre")
+// }else if (num_mes == 11){
+//   document.write ("Noviembre")
+// }else if (num_mes == 12){
+//   document.write("Diciembre")
+// }
+
+
+//punto 12
+// let num_semana = (prompt("ingrese el dia de la semana"))
+// if (num_semana == 1){
+//   document.write("El día de la semana es Lunes")
+// }else if (num_semana == 2){
+//   document.write("El día de la semana es Martes")
+// }else if (num_semana == 3){
+//   document.write("El día de la semana es Miércoles")
+// }else if (num_semana == 4){
+//   document.write("El día de la semana es Jueves")
+// }else{
+//   document.write("El día de la semana es Viernes")
+// }
